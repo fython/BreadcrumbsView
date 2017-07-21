c6fe7ae40e73e39d63073741031e0b6c38e7f5f6
Fung <fython@163.com>
2017-07-22 01:35:34 +0800

    Fix
    Signed-off-by: Fung <fython@163.com>

--- library/src/main/java/moe/feng/common/view/breadcrumbs/DefaultBreadcrumbsCallback.java
+++ library/src/main/java/moe/feng/common/view/breadcrumbs/DefaultBreadcrumbsCallback.java
@@ -7,7 +7,7 @@ public abstract class DefaultBreadcrumbsCallback implements BreadcrumbsCallback 
 	@Override
 	public void onItemClick(BreadcrumbsView view, int position) {
 		if (position == view.getItems().size() - 1) return;
-		view.removeItemBefore(position + 1);
+		view.removeItemAfter(position + 1);
 		this.onNavigateBack(view.getItems().get(position), position);
 	}
 
@@ -15,7 +15,7 @@ public abstract class DefaultBreadcrumbsCallback implements BreadcrumbsCallback 
 	public void onItemChange(BreadcrumbsView view, int parentPosition, Object nextItem) {
 		BreadcrumbItem nextBreadcrumb = view.getItems().get(parentPosition + 1);
 		nextBreadcrumb.setSelectedItem(nextItem);
-		view.removeItemBefore(parentPosition + 2);
+		view.removeItemAfter(parentPosition + 2);
 		if (parentPosition + 2 >= view.getItems().size()) {
 			view.notifyItemChanged(parentPosition + 1);
 		}
